1053ad061ad9473c66992391db38cbc0ba688dc3
salanton <salanton@gmail.com>
2025-04-15 11:44:30 +0300

    Try2

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+
+document.getElementById("root").innerHTML = `
+  <h1 style='font-family:sans-serif;font-size:24px'>✅ Simplex Калькулятор активен</h1>
+  <p>Это production-сборка. Компоненты успешно загружены.</p>
+`;
